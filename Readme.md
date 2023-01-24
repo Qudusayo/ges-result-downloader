d607849af91a20982e43f7cbad9d4c35042e02d2
Qudusayo <qqudusayo@gmail.com>
2023-01-24 15:01:55 +0100

    feat: init-commit

---
+++ Readme.md
@@ -0,0 +1,30 @@
+# Usage
+
+1. Clone the repository
+  ``` bash
+  git clone https://github.com/Qudusayo/ges-result-downloader.git
+  ```
+2. Navigate into the directory and install the required packages using yarn or npm
+  ```bash
+  cd ges-result-downloader
+
+  # Using yarn
+  yarn
+
+  # Using npm
+  npm install
+  ```
+3. Create `matrics.txt` file and enter the matric numbers in the file seperated by commas
+  ```js
+  // matrics.txt
+  000000,000001,000002,000004,000005,...
+  ```  
+4. Run `yarn start` or `npm start` to start the process and wait patiently for the process to be completed. **A good internet connection is required for quicker process.**  
+
+
+## Extras
+
+- You can open an issue [here](https://github.com/Qudusayo/ges-result-downloader/issues)
+- You can also contribute to the repo by making a [PR](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/about-pull-requests)
+
+  Thanks 
---
+++ automator.js
@@ -0,0 +1,73 @@
+require("chromedriver");
+const fs = require("fs");
+const { Builder, By, Key } = require("selenium-webdriver");
+
+const resultsDirectory = "./results";
+
+// Extract the matric numbers from the matrics.txt file
+const matricFileTxtContext = fs.readFileSync("./matrics.txt").toString();
+
+// Converts the numbers to arrays [00000,00001,00002...]
+const arrayOfMatricNums = matricFileTxtContext.split(",");
+
+//Check if results directory exist
+if (!fs.existsSync(resultsDirectory)) {
+  // Create result directory if it doesn't exist
+  fs.mkdirSync(resultsDirectory);
+}
+
+async function getGESResults() {
+  let driver = await new Builder().forBrowser("chrome").build();
+
+  try {
+    // Navigate to cgs url
+    await driver.get("https://cgs.ui.edu.ng/portal/index.php");
+    for (let index = 0; index < arrayOfMatricNums.length; index++) {
+      try {
+        // Enter the matric number  in it's input
+        await driver
+          .findElement(By.name("matric"))
+          .sendKeys(`${arrayOfMatricNums[index]}`);
+
+        // Enter the matric number as password keyboard action "Enter"
+        await driver
+          .findElement(By.name("passkey"))
+          .sendKeys(`${arrayOfMatricNums[index]}`, Key.ENTER);
+
+        // Find and click the result link on the side bar
+        await driver
+          .findElement(By.xpath("//a/span[contains(text(),'Result')]"))
+          .click();
+
+        // Scroll the page to the result div
+        let resultElement = await driver.findElement(By.className("invoice"));
+        await driver.executeScript(
+          "arguments[0].scrollIntoView(true);",
+          resultElement
+        );
+
+        // Take a screenshot of the result div
+        let encodedString = await resultElement.takeScreenshot(true);
+
+        // Save the screenshot named with the matric number in results directory
+        await fs.writeFileSync(
+          `./results/${arrayOfMatricNums[index]}.png`,
+          encodedString,
+          "base64"
+        );
+
+        // Logout the current logged in user
+        await driver.findElement(By.className("dropdown-toggle")).click();
+        await driver.findElement(By.name("logout")).click();
+      } catch (error) {
+        // Incase of any error, log the error and continur the process
+        console.log(error);
+      }
+    }
+  } finally {
+    // If all process are completed, close the driver
+    await driver.quit();
+  }
+}
+
+getGESResults();
